ffd6be9a21ec86912a41288c7413e6f83918170c
Ravi Waghmare <raviwaghmare2908@gmail.com>
2020-04-07 13:54:02 +0530

    added comaprison function

---
+++ comparison/comaprison.go
@@ -0,0 +1,13 @@
+package comparison
+
+import (
+	hwcc "hardware-classification-controller/api/v1alpha1"
+)
+
+func comparison(validatedHost map[string]map[string]interface{}, expectedHardwareprofile hwcc.ExpectedHardwareConfiguration) map[string]string {
+	validHost := make(map[string]string)
+
+	
+
+	return validHost
+}
